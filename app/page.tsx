80249d7917362b9e9f254b39823ca2555419837e
N Jayant Rao <njayantrao@gmail.com>
2026-02-02 19:12:23 +0530

    added hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="min-h-screen bg-black/96 antialiased bg-grid-white[0.02]">
-        
+      <main className="min-h-screen w-full bg-black/96 antialiased bg-grid-white[0.02]">
+        <HeroSection />
       </main>
     </div>
   );
