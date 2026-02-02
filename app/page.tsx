75925646973b9c8d39e428b9b219842499d9a1ff
N Jayant Rao <njayantrao@gmail.com>
2026-02-02 21:08:39 +0530

    added featured courses section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="min-h-screen w-full bg-black/96 antialiased bg-grid-white[0.02]">
         <HeroSection />
+        <FeaturedSection />
       </main>
     </div>
   );
